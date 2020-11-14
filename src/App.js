652032ac2b6342f1263aff3617d8b30f22153225
tomomanc4c <t.inoue@c4c.co.jp>
2020-11-14 21:38:54 +0900

    end

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 import SeachBar from './components/SearchBar';
+import ImageList from './components/ImageList';
+import Logo from './logo.png';
 
 const App = () => {
+  const [images, setImages] = useState([]);
+  const ApiKey = process.env.REACT_APP_PIXABAY_APIKEY
+  const onSearchSubmit = async (term) => {
+    try {
+      const params = {
+        key: ApiKey,
+        q: term,
+      };
+      const response = await axios.get("https://pixabay.com/api", { params });
+      setImages(response.data.hits);
+      if (response.data.total === 0) {
+        window.alert('お探しの画像はありません');
+      }
+    } catch {
+      window.alert('写真の取得に失敗しました');
+    }
+  };
   return (
-    <div>
-      <div>App</div>
-      <SeachBar />
+    <div className='ui container' style={ { marginTop: '25px' } }>
+      <img src={ Logo } alt='pixabay-logo' className='pixabay-logo' />
+      <SeachBar onSubmit={ onSearchSubmit } />
+      <ImageList images={ images } />
     </div>
-  )
-}
+  );
+};
 
 export default App;
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const SearchBar = () => {
+const SearchBar = ({ onSubmit }) => {
+  const [term, setTerm] = useState('');
+
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+    onSubmit(term);
+  }
   return (
-    <div>
-      <p>SearchBar</p>
+    <div className="ui segment">
+      <form onSubmit={ onFormSubmit } className="ui form">
+        <div className="field">
+          <label>Image Search</label>
+          <input type="text" name="search" placeholder="" value={ term } onChange={ (event) => {
+            setTerm(event.target.value);
+          }} />
+        </div>
+      </form>
     </div>
   )
-}
+};
 
 export default SearchBar;
